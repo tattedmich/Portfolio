c2d1003bc1fac9b87e1d305f8e413c8022c6d340
tattedmich <michlinaresdominguez@gmail.com>
2023-04-22 15:11:06 +0200

    update

--- js/shopping.js
+++ js/shopping.js
@@ -1,10 +1,3 @@
-class Shop {
-    constructor(shoppingBag){
-        this.bag = shoppingBag
-    }
-    getTotalAmount(){
-        if (this.bag.length > 0) {
-            return this.bag.reduce((acc, item) => acc + item.amount, 0)
-        }
-    }
-}
+const container = document.querySelector("div.container")
+const inputSearch = document.querySelector("input#inputSearch")
+const bag = []
